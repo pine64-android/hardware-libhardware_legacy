c157c7d83225fe184c47874273ab53f71f5f7268
Jon Smirl <jonsmirl@gmail.com>
2017-01-02 20:44:54 -0500

    exdroid-6.0.1_r3-a64-v2.0 release

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -34,7 +34,41 @@ endif
 ifdef WIFI_DRIVER_STATE_OFF
 LOCAL_CFLAGS += -DWIFI_DRIVER_STATE_OFF=\"$(WIFI_DRIVER_STATE_OFF)\"
 endif
+# realtek usb wifi module
+ifeq ($(BOARD_USR_WIFI), rtl8188eu)
+LOCAL_CFLAGS += -DRTL_8188EU_WIFI_USED
+LOCAL_CFLAGS += -DRTL_WIFI_VENDOR
+endif
 
+# realtek sdio wifi+bt module
+ifeq ($(BOARD_USR_WIFI), rtl8723bs)
+LOCAL_CFLAGS += -DRTL_8723BS_WIFI_USED
+LOCAL_CFLAGS += -DRTL_WIFI_VENDOR
+endif
+
+# realtek sdio wifi+bt module
+ifeq ($(BOARD_USR_WIFI), rtl8723bs_vq0)
+LOCAL_CFLAGS += -DRTL_8723BS_VQ0_WIFI_USED
+LOCAL_CFLAGS += -DRTL_WIFI_VENDOR
+endif
+
+# realtek sdio wifi+bt module
+ifeq ($(BOARD_USR_WIFI), rtl8723cs)
+LOCAL_CFLAGS += -DRTL_8723CS_WIFI_USED
+LOCAL_CFLAGS += -DRTL_WIFI_VENDOR
+endif
+
+# realtek sdio wifi module
+ifeq ($(BOARD_USR_WIFI), rtl8189fs)
+LOCAL_CFLAGS += -DRTL_8189FS_WIFI_USED
+LOCAL_CFLAGS += -DRTL_WIFI_VENDOR
+endif
+
+#espressif sdio wifi module
+ifeq ($(BOARD_USR_WIFI), esp8089)
+LOCAL_CFLAGS += -DESPRESSIF_ESP8089_WIFI_USED
+LOCAL_CFLAGS += -DESP_WIFI_VENDOR
+endif
 LOCAL_SRC_FILES += wifi/wifi.c
 
 ifdef WPA_SUPPLICANT_VERSION
